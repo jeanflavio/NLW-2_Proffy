573b9f3fffd491b9d958932583352390d3bf9834
Jean Flavio de Souza <jeansouz4@gmail.com>
2020-08-16 11:06:31 -0300

    agora vai

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,9 +4,8 @@ import routes from './routes'
 const app = express();
 
 app.use(express.json());
-<<<<<<< HEAD
+
 app.use(routes);
-=======
 
 
 app.get('/', (req, res) => {
@@ -14,6 +13,5 @@ app.get('/', (req, res) => {
     return res.json({ message: 'Hello Word'});
 })
 
->>>>>>> 3f122f13653f9ed3f83b56402650dc59891e5cf9
 
 app.listen(3005);
